e3df936e4e59f8e1eaa6edcde0b37744bd3ef98c
Filip Pýrek <PyrekFilip@gmail.com>
2023-03-05 14:05:07 +0100

    feat: basic Buttonize TS types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-export type A = {
-	a: 'b'
-	f: Function
+import type { Handler } from 'aws-lambda'
+
+export type ButtonizeEvent<Args extends Record<string, string> = {}> = {
+	timestamp: number
+	identity: {
+		user: {
+			id: string
+			email: string
+			firstName: string
+			lastName: string
+			groups: {
+				id: string
+				name: string
+			}[]
+		}
+		organization: {
+			id: string
+			name: string
+		}
+	}
+	args: Args
 }
 
-export const ts: A = {
-	a: 'b',
-	f: () => 'a'
+export type ButtonizeResponse = {
+	format: 'markdown' | 'json' | 'text'
+	body: string
 }
+
+export type ButtonizeHandler<Args extends Record<string, string> = {}> =
+	Handler<ButtonizeEvent<Args>, ButtonizeResponse>
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,40 @@
+import { Context } from 'aws-lambda'
 import { describe, it, expect } from 'vitest'
-import { ts } from '../src/index'
+import { ButtonizeHandler } from '../src/index'
 
 describe('index', () => {
-	it('should do', () => {
-		expect(ts.f()).toEqual('a')
+	it('should do', async () => {
+		const handler: ButtonizeHandler<{ a: string }> = async (event) => {
+			return {
+				format: 'json',
+				body: JSON.stringify(event)
+			}
+		}
+
+		await expect(
+			await handler(
+				{
+					timestamp: 12345,
+					args: {
+						a: 'b'
+					},
+					identity: {
+						user: {
+							email: 'asda@asda.cmo',
+							firstName: 'asda',
+							lastName: 'xzcz',
+							groups: [],
+							id: '1231231'
+						},
+						organization: {
+							name: 'asda',
+							id: 'sdascz'
+						}
+					}
+				},
+				{} as Context,
+				() => {}
+			)
+		)
 	})
 })
